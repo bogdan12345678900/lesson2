3b9b400c5166ddcd309dc2382669b8375c9a53f6
plosc_ea16 <plosinckijbogdan@gmail.com>
2024-10-30 17:15:15 +0200

    v 0.1

---
+++ main.py
@@ -0,0 +1,20 @@
+# size = 10
+# coice_user = input("a b c")
+# if coice_user == "a":
+#      for i in range(size):
+#          for j in range(size):
+#              if i == 0 or i == size - 1 or j == 0 or j == size - 1 or i ==j or i < j :
+#                  print("*", end="")
+#              else:
+#                  print(" ", end="")
+#          print()
+# elif coice_user == "b":
+#      for i in range(size):
+#          for j in range(size):
+#              if i == 0 or i == size - 1 or j == 0 or j == size - 1 or i ==j or j < i :
+#                  print("*", end="")
+#              else:
+#                  print(" ", end="")
+#          print()
+#
+
