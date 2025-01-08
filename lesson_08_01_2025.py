05eac55f6e85f79e14c712b00b70d9bf153103c7
plosc_ea16 <plosinckijbogdan@gmail.com>
2025-01-08 19:37:53 +0200

    v 0.3

---
+++ lesson_08_01_2025.py
@@ -0,0 +1,88 @@
+# a = 4
+# print(a/2)
+# print(a/a)
+# print(a/0)
+# print(c)
+# while True:
+#     try:
+#         a = int(input("a "))
+#         print(15/a)
+#     except ZeroDivisionError:
+#         a=1
+#         print(15/a)
+import math
+
+# while True:
+#     try:
+#         a = int(input("a "))
+#         print(15 / a)
+#     except BaseException:
+#         a = 1
+#         print(15 / a)
+#     except ValueError:
+#         print("NO")
+#         print(15 / 1)
+#     else:
+#         print("Else")
+#     finally:q
+#         print("Finally")
+
+
+# while True:
+#     try:
+#         a = int(input("a"))
+#         b = int(input("b"))
+#         print(a/b)
+#     except:
+#         print("Не можна ділити на нуль!!!!!!!")
+
+# def dilen(a,b):
+#     try:
+#         print(a/b)
+#     except ZeroDivisionError:
+#         print("Не можна ділити на нуль!!!!!!!")
+#
+#
+# a = 5
+# b = 0
+# dilen(a,b)
+
+
+#
+# while True:
+#     try:
+#         a = int(input("Число"))
+#         break
+#     except ValueError:
+#         print("Напиши цифрами")
+#
+#
+# while True:
+#     try:
+#         a = int(input("a"))
+#         print(math.sqrt(a))
+#     except BaseException as bs:
+#         print(f"Помилка {bs}")
+#         break
+
+
+
+#
+# res = {"Bogdan":"18" , "kiril" : "20" , "Sasha" : "12"}
+# user = int(input("1~5: "))
+# if user == 1:
+#     print(res)
+# elif user == 2:
+#     print(res)
+#     user2 = input("user2")
+#     print(res.pop(user2))
+# elif user == 3:
+#     user3 = input("")
+#     print(res)
+#     print(res.keys("Bogdan"))
+# elif user == 4:
+#     pass
+# elif user == 5:
+#     pass
+
+
